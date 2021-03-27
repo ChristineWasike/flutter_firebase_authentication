c88054daf34165b05f3fd879e0b6baa208dfa24c
ChristineWasike <christinewasike22@gmail.com>
2021-03-27 18:08:06 +0200

    Changes in gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_authentication/setup/login.dart';
 
-void main() {
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
--- lib/setup/login.dart
+++ lib/setup/login.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_authentication/pages/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -22,20 +23,41 @@ class _SignInState extends State<SignIn> {
       body: Form(
           child: Column(
         children: <Widget>[
-          Padding(padding: EdgeInsets.only(top: 40)),
-          Padding(padding: EdgeInsets.only(top: 30)),
+          Padding(padding: EdgeInsets.only(top: 20)),
           Padding(padding: EdgeInsets.only(bottom: 30)),
           Padding(
-            padding: EdgeInsets.all(20.0),
+            padding: EdgeInsets.only(left: 20.0, right: 20.0),
             child: TextFormField(
               controller: emailController,
               keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(labelText: "Email Address"),
+              decoration: InputDecoration(
+                icon: Icon(Icons.email),
+                hintText: 'Email',
+                labelText: 'Email *',
+              ),
               validator: (value) {
                 if (value.isEmpty) {
                   return 'Please Enter your email address';
                 } else if (!value.contains('@')) {
                   return 'Please enter a valid email address';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20.0, right: 20.0),
+            child: TextFormField(
+              controller: passwordController,
+              keyboardType: TextInputType.visiblePassword,
+              decoration: InputDecoration(
+                icon: Icon(Icons.lock),
+                hintText: 'Password',
+                labelText: 'Password *',
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please Enter your password';
                 }
                 return null;
               },
@@ -48,49 +70,18 @@ class _SignInState extends State<SignIn> {
                 print("Login pressed.");
                 print(emailController.text);
                 print(passwordController.text);
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return Home();
+                }));
               },
               child: Text(
                 'Login',
-                style: TextStyle(color: Colors.black),
+                style: TextStyle(color: Colors.white),
               ),
             ),
           )
         ],
       )),
-      // Column(
-      //   children: <Widget>[
-      //     SizedBox(
-      //       height: 20,
-      //     ),
-      //     Padding(
-      //       padding: const EdgeInsets.all(10.0),
-      //       child: TextFormField(
-      //         decoration: const InputDecoration(
-      //           icon: Icon(Icons.email),
-      //           hintText: 'Email',
-      //           labelText: 'Email *',
-      //         ),
-      //       ),
-      //     ),
-      //     Padding(
-      //       padding: const EdgeInsets.all(10.0),
-      //       child: TextFormField(
-      //         decoration: const InputDecoration(
-      //           icon: Icon(Icons.lock),
-      //           hintText: 'Password',
-      //           labelText: 'Password *',
-      //         ),
-      //       ),
-      //     ),
-      //     SizedBox(
-      //       height: 5,
-      //     ),
-      //     RaisedButton(
-      //       onPressed: () {},
-      //       child: const Text('Sign In', style: TextStyle(fontSize: 20)),
-      //     )
-      //   ],
-      // ),
     );
   }
 }
