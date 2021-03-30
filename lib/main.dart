f18997a7030f766c47b050365fc3b69c02a575f5
ChristineWasike <christinewasike22@gmail.com>
2021-03-30 16:02:38 +0200

    Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_authentication/authentication/authentication_service.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_authentication/setup/login.dart';
+import 'package:provider/provider.dart';
 
-
-Future<void> main() async{
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
@@ -13,14 +15,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Firebase Authentication',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: SignIn(),
-    );
+    return MultiProvider(
+        providers: [
+          Provider<AuthenticationService>(
+              create: (_)=> AuthenticationService(FirebaseAuth.instance),
+          ),
+          StreamProvider(
+              create: (context) => context.read<AuthenticationService>().authStateChanges,
+          ),
+        ],
+        child: MaterialApp(
+          title: 'Firebase Authentication',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          home: SignIn(),
+        ));
   }
 }
-
--- lib/setup/login.dart
+++ lib/setup/login.dart
@@ -1,3 +1,5 @@
+import 'dart:js';
+
 import 'package:firebase_authentication/pages/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -16,72 +18,78 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Sign In"),
-      ),
-      body: Form(
-          child: Column(
-        children: <Widget>[
-          Padding(padding: EdgeInsets.only(top: 20)),
-          Padding(padding: EdgeInsets.only(bottom: 30)),
-          Padding(
-            padding: EdgeInsets.only(left: 20.0, right: 20.0),
-            child: TextFormField(
-              controller: emailController,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                icon: Icon(Icons.email),
-                hintText: 'Email',
-                labelText: 'Email *',
-              ),
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please Enter your email address';
-                } else if (!value.contains('@')) {
-                  return 'Please enter a valid email address';
-                }
-                return null;
-              },
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 20.0, right: 20.0),
-            child: TextFormField(
-              controller: passwordController,
-              keyboardType: TextInputType.visiblePassword,
-              decoration: InputDecoration(
-                icon: Icon(Icons.lock),
-                hintText: 'Password',
-                labelText: 'Password *',
-              ),
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please Enter your password';
-                }
-                return null;
-              },
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(20.0),
-            child: ElevatedButton(
-              onPressed: () {
-                print("Login pressed.");
-                print(emailController.text);
-                print(passwordController.text);
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return Home();
-                }));
-              },
-              child: Text(
-                'Login',
-                style: TextStyle(color: Colors.white),
+
+    final firebaseUser = context.watch<User>;
+
+    if (firebaseUser != null) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("Sign In"),
+        ),
+        body: Form(
+            child: Column(
+          children: <Widget>[
+            Padding(padding: EdgeInsets.only(top: 20)),
+            Padding(padding: EdgeInsets.only(bottom: 30)),
+            Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 20.0),
+              child: TextFormField(
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.email),
+                  hintText: 'Email',
+                  labelText: 'Email *',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please Enter your email address';
+                  } else if (!value.contains('@')) {
+                    return 'Please enter a valid email address';
+                  }
+                  return null;
+                },
               ),
             ),
-          )
-        ],
-      )),
-    );
+            Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 20.0),
+              child: TextFormField(
+                controller: passwordController,
+                keyboardType: TextInputType.visiblePassword,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.lock),
+                  hintText: 'Password',
+                  labelText: 'Password *',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please Enter your password';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  print("Login pressed.");
+                  print(emailController.text);
+                  print(passwordController.text);
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return Home();
+                  }));
+                },
+                child: Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        )),
+      );
+    }
+    return Text("Not signed in");
   }
 }
