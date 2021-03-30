8e94089384233a9743cfebd4a40574497ebb085c
ChristineWasike <christinewasike22@gmail.com>
2021-03-30 20:48:19 +0200

    Fixed authentication

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_authentication/pages/home.dart';
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -8,6 +8,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final generateAutoWord = new WordPair.random();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +18,7 @@ class _HomeState extends State<Home> {
       ),
       body: Column(
         children: [
-          Text("Home"),
+          Text(generateAutoWord.asCamelCase),
         ],
       ),
     );
